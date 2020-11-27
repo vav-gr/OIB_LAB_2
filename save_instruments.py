cb6576a79a04082696fbde0701635342bafc7f4b
ivanshin <48406105+ivanshin@users.noreply.github.com>
2020-11-27 13:25:16 +0300

    Update save_instruments.py

--- save_instruments.py
+++ save_instruments.py
@@ -30,4 +30,41 @@ def save_original_image(image, file_name):
 
     path = directory + file_name
     cv2.imwrite(path, image)
+    
+    
+
+
+# except Exception as e:
+# print("Oops!", e.__class__, "occurred.")
+
+
+def save_edited_image(base_image, output_image_name):
+    try:
+        if os.path.exists('Edited') == 0:
+            os.mkdir('Edited')
+            os.chmod(r'Edited', 0o777)
+        directory = os.path.abspath(os.curdir) + '\\Edited\\'
+        list_of_files = os.listdir(directory)
+        copy_counter = 0
+        i = 0
+        index = output_image_name.find('.')
+        if len(list_of_files) != 0:
+            while True:
+                if list_of_files[i] == output_image_name or list_of_files[i] == output_image_name[:index] + str(
+                        copy_counter) + output_image_name[index:]:
+                    copy_counter = copy_counter + 1
+                i = i + 1
+                if i >= len(list_of_files):
+                    break
+
+        if copy_counter > 0:
+            output_image_name = output_image_name[:index] + str(copy_counter) + output_image_name[index:]
+
+        path = directory + output_image_name
+
+        base_image.save(path)
+    except Exception as e:
+        print("Oops!", e.__class__, "occurred.")
+
+        
 
