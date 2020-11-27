1b7e4cd034014ae5fd9e828271bf3945b16072b1
Maxim1017 <48377957+Maxim1017@users.noreply.github.com>
2020-11-27 13:50:05 +0300

    Update server.py

--- server.py
+++ server.py
@@ -19,3 +19,20 @@ app.debug = True
 @app.route('/api/test', methods=['POST'])
 def test():
     r = request
+    
+   
+    # convert string of image data to uint8
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    filename='prinytoe.jpg'
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    save_original_image(img,filename)
+    add_to_origin_table(filename)
+    path_to_original = os.path.abspath(os.curdir) + '\\Original\\'+ filename
+    recieved_counter = 0
+    edited_name = 'edited' + str(recieved_counter) + '.jpg'
+    path_to_edited = os.path.abspath(os.curdir) + '\\Edited\\'+ edited_name
+
+    watermark_photo(path_to_original, edited_name, '2.jpg', position=(1, 1))
+    recieved_counter = recieved_counter + 1
+   
