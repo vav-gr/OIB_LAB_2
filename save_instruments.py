42e6c7fc04e257d5e698cbf70bd51040ed1d8101
ivanshin <48406105+ivanshin@users.noreply.github.com>
2020-11-27 13:28:30 +0300

    Update save_instruments.py

--- save_instruments.py
+++ save_instruments.py
@@ -32,39 +32,6 @@ def save_original_image(image, file_name):
     cv2.imwrite(path, image)
     
     
-    
-def add_to_origin_table(file_name):
-    try:
-        engine = create_engine('sqlite:///RECEIVED.db', echo=True)
-        current_datetime = datetime.now()
-        conn = engine.connect()
-        meta_d = MetaData(engine)
-        if not engine.dialect.has_table(engine, 'Original'):
-            table = Table(
-                'Original', meta_d,
-                Column('id', Integer, primary_key=True),
-                Column('file_name', String, nullable=False),
-                Column('date_time', String, nullable=False),
-            )
-            meta_d.create_all(engine)
-
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        table = Table('Original', meta_d, autoload=True)
-        result = session.query(table).all()
-        copy_counter = 0
-        i = 0
-
-        index = file_name.find('.')
-        if len(result) != 0:
-            while True:
-                if result[i][1] == file_name or result[i][1] == file_name[:index] + str(copy_counter) + file_name[
-                                                                                                        index:]:
-                    copy_counter = copy_counter + 1
-                i = i + 1
-                if i >= len(result):
-                    break
-
 
 # except Exception as e:
 # print("Oops!", e.__class__, "occurred.")
@@ -180,6 +147,16 @@ def add_to_edited_table(file_name):
     except Exception as e:
         print("Oops!", e.__class__, "occurred.")
 
+        
+def test():
+    print('hi')
+    add_to_origin_table('image.png')
+    add_to_edited_table('image.png')
+    im = Image.open('image.png')
+    save_original_image(im, 'im.png')
+    save_edited_image(im, 'im.png')
+
+# test()
 
         
 
